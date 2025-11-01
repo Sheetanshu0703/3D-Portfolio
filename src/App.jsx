3f33196bff7426b6d5b36274d0df5481f7e2f673
Sheetanshu0703 <sheetanshuyuvi.0703@gmail.com>
2025-11-01 18:16:45 +0530

    Initial commit - 3D portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/projects' element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+
+
+    </main>
+  )
+}
+
+export default App 
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Services from './Services';
+import Projects from './Projects';
+import Blog from './Blog';
+
+export { Home, About, Contact, Services, Projects, Blog };
